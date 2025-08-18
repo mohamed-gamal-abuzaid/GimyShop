44ec1f259d424cabce2fc3c4ccc1ec46d3ea7f59
Mohamed Gamal <mohamed-gamal-20190427>
2025-08-18 05:58:17 +0300

    v3

--- components/Home/Navbar/MobielNav.tsx
+++ components/Home/Navbar/MobielNav.tsx
@@ -14,13 +14,13 @@ const navClass = showNav ? "translate-x-0" : "-translate-x-full";
 
   return (
     <div>
-      <div className={`fixed ${navClass} inset-0 transform transition-all duration-500 z-[1002] bg-black opacity-50 w-full h-screen`}>
-        <div className="text-white fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-black space-y-6 z-[1050]">
+      <div className={`fixed ${navClass} inset-0 transform transition-all duration-500 z-[1002] bg-pink-850 opacity-85 w-full h-screen`}>
+        <div className="text-white fixed pt-36 gap-2.5  flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-black space-y-6 z-[1050]">
           {navLinks.map((link) => (
             <Link
               key={link.id}
               href={link.url}
-              className={`text-white ${navClass} w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30]`}
+              className={`text-white ${navClass} w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30] hover:text-green-500 cursor-pointer`}
             >  
               <p>{link.label}</p>
             </Link>
